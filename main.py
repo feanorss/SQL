426c45c57f8e26d1ef4f583d9c4095169ecb6844
feanorss <madro.milos@gmail.com>
2024-03-26 18:43:23 +0100

    SQL

---
+++ main.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname = "bphzurc4badokyq3ezig",
+    user = "ucrksoznquj3b71nfqcv",
+    password = "2YKt1zZxmkqsYgV41W3260QJNXRTn6",
+    host = "bphzurc4badokyq3ezig-postgresql.services.clever-cloud.com",
+    port = "50013"
+)
+
+print(conn)
+
+cursor = conn.cursor()
+
+print(cursor)
+
+cursor.execute("SELECT * FROM authors")
+autori = cursor.fetchall()
+print(autori)
+
+cursor.execute("SELECT * FROM authors")
+prvy_autor = cursor.fetchone()
+print(prvy_autor)
+
